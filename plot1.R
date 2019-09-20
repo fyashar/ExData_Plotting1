99f4a2089cfdae77e0e65c8580cf6b463143360c
Yashar Farajollahi <yasharf_2000@yahoo.com>
2019-09-19 22:16:44 -0600

    Dataset created.

--- plot1.R
+++ plot1.R
@@ -1,1 +1,10 @@
-## testing github connection
+## Reading raw data from "household_power_consumption.txt" file.
+raw.data <- read.table(file = "./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+## Converting Time and Date string variables using dplyr "mutate" function, and "dmy" and "hms" functions from lubridate package.
+library(dplyr)
+library(lubridate)
+tidy.data <- raw.data %>% tbl_df() %>% mutate(Date = dmy(Date), Time = hms(Time))
+
+## Filtering data to obtain a dataset for dates 2007-02-01 and 2007-02-02
+filtered.data <- filter(tidy.data, Date >= "2007-02-01", Date <= "2007-02-02")
