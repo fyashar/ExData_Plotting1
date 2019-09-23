2da2b480586951ef2caece0257b8fab614a5417e
Yashar Farajollahi <yasharf_2000@yahoo.com>
2019-09-22 21:16:54 -0600

    Third plot added.

---
+++ plot3.R
@@ -0,0 +1,22 @@
+## Reading raw data from "household_power_consumption.txt" file.
+raw.data <- read.table(file = "./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+## Converting Time and Date string variables using dplyr "mutate" function, and "dmy" and "hms" functions from lubridate package.
+## A new column, "Date_Time", is also added to show Date and Time combined.
+library(dplyr)
+library(lubridate)
+tidy.data <- raw.data %>% tbl_df() %>% mutate(Date = dmy(Date), Time = hms(Time), Date_Time = dmy_hms(paste(raw.data$Date, raw.data$Time)))
+
+## Filtering data to obtain a dataset for dates 2007-02-01 and 2007-02-02
+filtered.data <- filter(tidy.data, Date >= "2007-02-01", Date <= "2007-02-02")
+
+## Opening a png file
+png(filename = "plot3.png", width = 480, height = 480)
+## Creating a plot
+plot(x = filtered.data$Date_Time, y = filtered.data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(x = filtered.data$Date_Time, y =  filtered.data$Sub_metering_2, type = "l", col = "red")
+lines(x = filtered.data$Date_Time, y =  filtered.data$Sub_metering_3, type = "l", col = "blue")
+## Adding Legend
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"), cex = 0.75)
+## Closing the file
+dev.off()
